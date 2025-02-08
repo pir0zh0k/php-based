b92880b23cf866ab8f605d5673396886a424fe87
Paul <designer1812paul@gmail.com>
2025-02-08 18:02:35 +0300

    Типы данных

--- index.php
+++ index.php
@@ -6,11 +6,38 @@
     <title>PHP</title>
 </head>
 <body>
-    <?php 
-        $num = 10;
-        echo $num;
-        $num = 20;
-        echo $num;
+    <?php
+    // $num = 28;
+    // $num = 0b11100;
+    // $num = 034;
+    // $num = 0x1C;
+
+    // $num = 1.15;
+
+    // $isLesson = true;
+
+    // $a = 10;
+    // $b = 5;
+
+    // $result = "$a + $b";
+
+    // echo $result;
+
+    // $result = '$a + $b';
+
+    // echo $result;
+
+    // $text = "Модель \"Apple 2\"";
+
+    // echo $text;
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $a = NULL;
+
+    echo $a;
     ?>
 </body>
 </html>
