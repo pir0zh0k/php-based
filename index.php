09d99b99b1b9a47c3706173f8f9b0ef92e1ffc04
Paul <designer1812paul@gmail.com>
2025-02-12 22:58:20 +0300

    Установка типа переменной. Преобразование типов

--- index.php
+++ index.php
@@ -7,18 +7,12 @@
 </head>
 <body>
     <?php
-    $a = 10;
-    $b = "20";
+    $var = 22;
+    $strVar = (string)$var;
 
-    echo is_integer($a);
-    echo is_string($a);
-    echo is_double($a);
-    echo is_numeric($a);
-    echo is_bool($a);
-    echo is_scalar($a);
-    echo is_null($a);
-    echo is_array($a);
-    echo is_object($a);
+    echo gettype($var);
+    echo "<br/>";
+    echo gettype($strVar);
     ?>
 </body>
 </html>
