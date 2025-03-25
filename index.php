36c06b1a62adcc2061c947e6e320bca650ec531d
Paul <designer1812paul@gmail.com>
2025-03-25 13:17:40 +0300

    Виды массивов

--- index.php
+++ index.php
@@ -7,11 +7,27 @@
 </head>
 <body>
     <?php
-        $arr = Array("Hello", "Goodbye");
-        $arr2 = ["start", "end"];
+        // $phone = ["name" => "IPhone", "price" => 80000, "100Гц"];
+        // var_dump($phone);
+        // echo $phone["name"];
+        // echo "<br/>";
+        // echo $phone[0];
 
-        echo $arr;
-        echo $arr2;
+        // $friends = [["Alice", "Tom"],["Kate", "Bob"]];
+        // var_dump($friends);
+        // echo $friends[0][0];
+
+        $car = [
+            "name" => "Audi",
+            "specifications" => [
+                "engine_power" => 150,
+                "engine_litre" => 6,
+            ]
+        ];
+
+        var_dump($car);
+
+        echo $car["specifications"]["engine_power"];
     ?>
 </body>
 </html>
