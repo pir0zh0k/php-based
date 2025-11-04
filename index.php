d1962262a732ea549118f035968fa0d87c6020eb
Залюбовин Павел <p.zalyubovin@napoleonit.ru>
2025-11-04 11:20:59 +0300

    Стрелочные функции

--- index.php
+++ index.php
@@ -7,15 +7,34 @@
 </head>
 <body>
     <?php
-        $a = 8;
-        $b = 10;
+        // $a = 10;
+        // $b = 8;
 
-        $showNumber = function($c) use($a, $b) {
-            return $a + $b +$c;
-        };
+        // $sum = fn($c) => $a + $b + $c;
 
-        $result = $showNumber(20);
-        echo $result;
+        // $result = $sum(10);
+
+        // echo $result;
+
+        function sum($numbers, $condition) {
+            $result = 0;
+
+            foreach($numbers as $number) {
+                if($condition($number)) {
+                    $result += $number;
+                }
+            }
+
+            return $result;
+        }
+
+        $myNumbers = [-2, -1, 0, 1, 2, 3, 4];
+
+        $positiveSum = sum($myNumbers, fn($n) => $n > 0);
+
+        $eventSum = sum($myNumbers, fn($n) => $n % 2 === 0);
+
+        echo "Сумма положительных чисел: " . $positiveSum . "<br/>" . "Сумма четных чисел: " . $eventSum;
     ?>
 </body>
 </html>
