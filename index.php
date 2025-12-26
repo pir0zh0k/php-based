d495aca535ece5a4aa2c1e41d96b0a3d5aaae166
Залюбовин Павел <p.zalyubovin@napoleonit.ru>
2025-12-26 10:36:04 +0300

    Генератор паролей

--- index.php
+++ index.php
@@ -7,34 +7,45 @@
 </head>
 <body>
     <?php
-        // $tom = "Tom";
-        // $sam = &$tom;
+        function generatePassword(int $upperCount = 2, int $lowerCount = 5, int $digitCount = 3, int $specialCount = 2) {
+            $uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+            
+            $lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+            $digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    
+            $special = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+'];
 
-        // $sam = "Sam";
+            $password = [];
 
-        // echo "Tom = " . $tom;
-        // echo "Sam = " . $sam;
+            for($i = 0; $i < $upperCount; $i++) {
+                $randomIndex = array_rand($uppercase);
+                $password[] = $uppercase[$randomIndex];
+            }
 
-        // function square(&$a) {
-        //     $a *= $a;
-        //     echo "a = " . $a;
-        // }
+            for($i = 0; $i < $lowerCount; $i++) {
+                $randomIndex = array_rand($lowercase);
+                $password[] = $lowercase[$randomIndex];
+            }
 
-        // $number = 5;
+            for($i = 0; $i < $digitCount; $i++) {
+                $randomIndex = array_rand($digits);
+                $password[] = $digits[$randomIndex];
+            }
 
-        // square($number);
-        // echo "<br/> Number = " . $number;
+            for($i = 0; $i < $specialCount; $i++) {
+                $randomIndex = array_rand($special);
+                $password[] = $special[$randomIndex];
+            }
 
-        function &checkName(&$name) {
-            if($name === "admin") $name = "Tom";
-            return $name;
+            shuffle($password);
+
+            return implode('', $password);
         }
 
-        $userName = "admin";
-        $checkedName = &checkName($userName);
-
-        echo "<br/> userName = " . $userName;
-        echo "<br/> checkedName = " . $checkedName;
+        echo generatePassword()
     ?>
 </body>
 </html>
